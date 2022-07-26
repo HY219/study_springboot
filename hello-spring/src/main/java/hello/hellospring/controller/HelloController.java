91d5a6d3af65212d517d9e24804e0f012ade59aa
hye219 <jhy_0219@daum.net>
2022-07-26 18:08:01 +0900

    정적 컨텐츠, MVC와 템플릿 엔진

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "Spring!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model){ //name 이 입력된 값으로 변경(ex.Spring!!!)
+        model.addAttribute("name", name); //name 이 입력된 값으로 변경(ex.Spring!!!)
+        return "hello-template";
     }
 }
