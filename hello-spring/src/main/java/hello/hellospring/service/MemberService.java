47db71ea2d8889b9f5441864ce3fba54c2c17462
hye219 <jhy_0219@daum.net>
2022-08-01 14:00:10 +0900

    회원가입서비스_Optional을 바로 반환하지 않도록 수정

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -13,6 +13,7 @@ public class MemberService {
     /**
      * 회원가입
      */
+    /*
     public Long join(Member member){
         //같은 이름이 있는 중복 회원x
         //memberRepository.findByName(member.getName()); //memberRepository에서 findByName으로 찾아온 member.getName()
@@ -22,6 +23,21 @@ public class MemberService {
         result.ifPresent(m-> {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         });
+
+        memberRepository.save(member); //memberRepository의 save를 호출
+        return member.getId();
+    }
+     */
+
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원x
+        //memberRepository.findByName(member.getName()); //memberRepository에서 findByName으로 찾아온 member.getName()
+        //ctrl + Alt + v => return을 바로 반환해준다.
+        memberRepository.findByName(member.getName())
+                //값이 있으면 throw new를 반환
+                .ifPresent(m-> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
 
         memberRepository.save(member); //memberRepository의 save를 호출
         return member.getId();
